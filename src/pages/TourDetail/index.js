d76acf725f2f9bc07db2e5ff75a60c212e9e2dce
toan <toan130812@gmail.com>
2022-11-02 21:12:11 +0700

    anh

--- src/pages/TourDetail/index.js
+++ src/pages/TourDetail/index.js
@@ -2,10 +2,10 @@ import Goituvan from "./Giotuvan";
 import Mota from "./Mota";
 import Layout from "../../layout/Default";
 import SlideImage from "./slideimage";
-import img1 from '../../assets/images/sec_inbound_tour_bg.jpg'
-import img2 from '../../assets/images/sec_inbound_tour_bg.jpg'
-import img3 from '../../assets/images/sec_inbound_tour_bg.jpg'
-import img4 from '../../assets/images/sec_inbound_tour_bg.jpg'
+import img1 from '../../assets/images/1.jpg'
+import img2 from '../../assets/images/2.jpg'
+import img3 from '../../assets/images/3.jpg'
+import img4 from '../../assets/images/4.jpg'
 import { Row } from "react-bootstrap";
 
 const imageArr=[
