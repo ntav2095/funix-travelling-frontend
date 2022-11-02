6d5d07a48f9f917911a2ec981129c304d3f2a810
ntav <ntav2095@gmail.com>
2022-11-02 12:57:47 +0700

    x

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,1 +1,5 @@
 export const phoneNumber = require("./phone-number.png");
+export const travelPic = require("./sec_inbound_tour_bg.jpg");
+export const one = require("./1.jpg");
+export const visaBanner = require("./visa-banner.jpg");
+export const worldMap = require("./world-map.jpg");
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 // css bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// react slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
