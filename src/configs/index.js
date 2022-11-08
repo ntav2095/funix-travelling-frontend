4285c006533891a35b11a6886619415cfd234962
ntav <ntav2095@gmail.com>
2022-11-08 16:33:48 +0700

    x

--- src/configs/index.js
+++ src/configs/index.js
@@ -7,10 +7,6 @@ if (process.env.NODE_ENV === "production") {
   config = productionConfig;
 }
 
-<<<<<<< HEAD
-export default config;
-=======
-export default productionConfig;
+// export default productionConfig;
 
-// export default config;
->>>>>>> firebase-test
+export default config;
