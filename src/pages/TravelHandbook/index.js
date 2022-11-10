e580ca9c43b859544606f51fd7f1b29b62e72c98
ntav <ntav2095@gmail.com>
2022-11-10 22:03:00 +0700

    x

--- src/pages/TravelHandbook/index.js
+++ src/pages/TravelHandbook/index.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { postsApi } from "../../services/apis";
 import { getDate, getMonth, getYear } from "date-fns";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
-import { Row, Col } from "react-bootstrap";
 
 // components
 import Layout from "../../layout/Default";
@@ -29,7 +28,6 @@ function TravelHandbook() {
   }
 
   function contentDes(content) {
-    console.log(content);
     let description = { text: [], image: [] };
     content.map((item) => {
       let t =
@@ -60,41 +58,39 @@ function TravelHandbook() {
             <Breadcrumb.Item active>CẨM NANG DU LỊCH</Breadcrumb.Item>
           </Breadcrumb>
 
-          <div className={classes.travelHandbook}>
-            <Row lg="3" md="2" sm="1">
-              {data
-                ? data.items.map((item) => (
-                    <Col key={item._id} className="mb-4">
-                      <Link
-                        className={classes.story}
-                        key={item._id}
-                        to={`/cam-nang-du-lich/${item._id}`}
-                      >
-                        <div className={classes.inner}>
-                          <div
-                            className={classes.image}
-                            style={{
-                              backgroundImage: `url(${
-                                contentDes(item.content).image[0]
-                              })`,
-                            }}
-                          ></div>
-                          <div className={classes.boxText}>
-                            <h2 className={classes.title}>{item.title}</h2>
-                            <p className={classes.date}>
-                              {date(item.updatedAt || item.createdAt)}
-                            </p>
-                            <p className={classes.desc}>
-                              {contentDes(item.content).text[0].slice(0, 100)}
-                              ...
-                            </p>
-                          </div>
-                        </div>
-                      </Link>
-                    </Col>
-                  ))
-                : null}
-            </Row>
+          <div className="row">
+            {data &&
+              data.items.length > 0 &&
+              data.items.map((item) => (
+                <div key={item._id} className="col-12 col-md-6 col-lg-4">
+                  <Link
+                    className={classes.story}
+                    key={item._id}
+                    to={`/cam-nang-du-lich/${item._id}`}
+                  >
+                    <div className={classes.inner}>
+                      <div
+                        className={classes.image}
+                        style={{
+                          backgroundImage: `url(${
+                            contentDes(item.content).image[0]
+                          })`,
+                        }}
+                      ></div>
+                      <div className={classes.boxText}>
+                        <h2 className={classes.title}>{item.title}</h2>
+                        <p className={classes.date}>
+                          {date(item.updatedAt || item.createdAt)}
+                        </p>
+                        <p className={classes.desc}>
+                          {contentDes(item.content).text[0].slice(0, 100)}
+                          ...
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
       </Layout>
--- src/pages/TravelHandbookDetail/index.js
+++ src/pages/TravelHandbookDetail/index.js
@@ -19,86 +19,96 @@ import quillGetHTML from "../../services/helpers/quillGetHTML";
 import { getDate, getMonth, getYear } from "date-fns";
 
 function TravelHandbookDetail() {
-  const [state,setState]=useState()
+  const [state, setState] = useState();
   const [sendRequest, isLoading, data, error] = useAxios();
   const quill = useRef();
-  const {id}=useParams()
-  console.log(state)
-  function date(dateString){
-    const dateStringtoformater= new Date(dateString)
-    const day=getDate(dateStringtoformater)
-    const month=getMonth(dateStringtoformater)
-    const year=getYear(dateStringtoformater)
-    return `${day}-${month}-${year}`
+  const { id } = useParams();
+  console.log(state);
+  function date(dateString) {
+    const dateStringtoformater = new Date(dateString);
+    const day = getDate(dateStringtoformater);
+    const month = getMonth(dateStringtoformater);
+    const year = getYear(dateStringtoformater);
+    return `${day}-${month}-${year}`;
   }
-  function contentDes(content){
-    
-    let description={text:[],image:[]}
-    content.map(item=>{
-      let t= typeof item.insert === 'string' && item.insert.length>10?description.text.push(item.insert):null
-      let e= item.insert.image?description.image.push(item.insert.image):null
-      return item
-    })
-    return description
+  function contentDes(content) {
+    let description = { text: [], image: [] };
+    content.map((item) => {
+      let t =
+        typeof item.insert === "string" && item.insert.length > 10
+          ? description.text.push(item.insert)
+          : null;
+      let e = item.insert.image
+        ? description.image.push(item.insert.image)
+        : null;
+      return item;
+    });
+    return description;
   }
 
-  useEffect(()=>{
-    sendRequest(postsApi.get())
-  },[])
   useEffect(() => {
-    if(data){
-      let posts=data.items.filter(item=>item._id===id)
-      console.log('dữ liệu')
-      console.log(posts,data)
-      setState(posts[0])
+    sendRequest(postsApi.get());
+  }, []);
+  useEffect(() => {
+    if (data) {
+      let posts = data.items.filter((item) => item._id === id);
+      console.log("dữ liệu");
+      console.log(posts, data);
+      setState(posts[0]);
     }
-    
-  }, [data])
-  
+  }, [data]);
+
   useEffect(() => {
-    if(state){
-    quill.current.innerHTML = quillGetHTML({ops:state.content});}
+    if (state) {
+      quill.current.innerHTML = quillGetHTML({ ops: state.content });
+    }
   }, [state]);
   usePageTitle(`Cẩm nang --- đang cập nhật || Go Travel`);
 
   return (
     <Layout sidebarRight primary>
-      <div className={classes.story}>
-        {state?
-        <div className={classes.storyHeader}>
-          <h1>{state.title}</h1>
-          <p className={classes.date}>
-            Posted on <span>{date(state.updatedAt||state.createdAt)}</span> by{" "}
-            <Link to="/admin">admin</Link>
-          </p>
-        </div>
-        :null}
+      <div className={classes.x}>
+        {state ? (
+          <div className={classes.storyHeader}>
+            <h1>{state.title}</h1>
+            <p className={classes.date}>
+              Posted on <span>{date(state.updatedAt || state.createdAt)}</span>{" "}
+              by <Link to="/admin">admin</Link>
+            </p>
+          </div>
+        ) : null}
         <div className={classes.storyContent}>
-        <div className={classes.quillContent} ref={quill}></div>
+          <div className={classes.quillContent} ref={quill}></div>
         </div>
 
         <div className={classes.relatedStories}>
           <p className={classes.relatedStoriesTitle}>Bài viết liên quan</p>
           <ul>
-            {data?data.items.map((item) => (
-              <li key={item.id} className={classes.relatedStory}>
-                <Link to={`/cam-nang-du-lich/${item._id}`} >
-                  <div className={classes.relatedStory}>
-                    <div
-                      className={classes.image}
-                      style={{ backgroundImage: `url(${contentDes(item.content).image[0]})` }}
-                    >
-                      {/* <img src={item.image} alt={item.title} /> */}
-                    </div>
+            {data
+              ? data.items.map((item) => (
+                  <li key={item.id} className={classes.relatedStory}>
+                    <Link to={`/cam-nang-du-lich/${item._id}`}>
+                      <div className={classes.relatedStory}>
+                        <div
+                          className={classes.image}
+                          style={{
+                            backgroundImage: `url(${
+                              contentDes(item.content).image[0]
+                            })`,
+                          }}
+                        ></div>
 
-                    <div className={classes.textBox}>
-                      <h4>{item.title}</h4>
-                      <p className={classes.date}>{date(item.updatedAt||item.createdAt)}</p>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-            )):null}
+                        <div className={classes.textBox}>
+                          <h4>{item.title}</h4>
+                          <p className={classes.date}>
+                            {date(item.updatedAt || item.createdAt)}
+                          </p>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                ))
+              : null}
           </ul>
         </div>
       </div>
