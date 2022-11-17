8622b14547211bc3c42fa293580f348d74fcaaf3
toan <toan130812@gmail.com>
2022-11-17 21:08:48 +0700

    ...

--- src/pages/HomeNew/tour/index.js
+++ src/pages/HomeNew/tour/index.js
@@ -3,72 +3,81 @@ import styles from './tour.module.css'
 import { brokenImage } from "../../../assets/images";
 import { useNavigate } from "react-router-dom";
 import { click } from "@testing-library/user-event/dist/click";
+import Slider from "react-slick";
 
 
 function Tour(props){
     const {title,tour,naviga,isLoading}=props
     const navigation=useNavigate()
+    const  settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          }
+        ]
+      };
     const handlerBrokenImg = (e) => {
         e.target.src = brokenImage;
       };
     
-const click=(e)=>{
-    const scroll= document.getElementById('scroll')
-     if(e){
-        document.documentElement.scrollLeft
-        scroll.scrollLeft=scroll.scrollLeft - 100
-    }else{
-        scroll.scrollLeft=scroll.scrollLeft + 100
 
-}}
 
-useEffect(()=>{
-    const scroll= document.getElementById('scroll')
-    scroll.addEventListener('scroll',()=>{
-        console.log(scroll.scrollLeft)
-        scroll.scrollLeft=scroll.scrollLeft + 100
-    }
-    )
-},[])
+
 
 return(
   <div className={styles.title}>
     <div>
         <h3>{title}</h3>
     </div>
-    <div className={styles.position}>
-        <div id="scroll" className={styles.container} >
-            <button className={styles.button +' '+ styles.after} onClick={()=>click(0)}>{'<'}</button>
-            {!isLoading &&
-                tour &&
-                tour.map((item, id) => (
-                <div
-                    key={id}
-                    className={styles.carouselItem}
-                    onClick={() => {}}
-                >
-                    <div className={styles.img}>
-                        <img src={item.thumb} alt={tour.name} onError={handlerBrokenImg} />
-                    </div>
-                    <div className={styles.content}>
-                        <h6>item.name</h6>
-                        <ul>
-                            <li>{item.journey}</li>
-                            <li>{item.days+ ' ngày ' + item.nights +' đêm'}</li>
-                            <li>{'Trọn gói:' + item.currentPrice + 'đ'}</li>
-                        </ul>
-                    </div>
+    <div className={styles.container} >
+    <Slider {...settings}>
+        {!isLoading &&
+            tour &&
+            tour.map((item, id) => (
+            <div
+                key={id}
+                className={styles.carouselItem}
+                onClick={() => {}}
+            >
+                <div className={styles.img}>
+                    <img src={item.thumb} alt={tour.name} onError={handlerBrokenImg} />
                 </div>
-                ))}
-
-            {isLoading && (
-                <div className={styles.carouselItem + " " + styles.placeholder}></div>
-            )}
-            <button className={styles.button +' '+ styles.befor} onClick={()=>click(1)}>{'>'}</button>
-        </div>
+                <div className={styles.content}>
+                    <h6>item.name</h6>
+                    <ul>
+                        <li>{item.journey}</li>
+                        <li>{item.days+ ' ngày ' + item.nights +' đêm'}</li>
+                        <li>{'Trọn gói:' + item.currentPrice + 'đ'}</li>
+                    </ul>
+                </div>
+            </div>
+            ))}
+    </Slider>
     </div>
     <div className={styles.tourdetail} onClick={()=>navigation(naviga)}>Xem tất cả</div>
   </div>
 )
 }
 export default Tour
+
+
