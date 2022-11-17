9cc1b7515c4577d930954b3af54fb863bce1a82a
ntav <ntav2095@gmail.com>
2022-11-17 23:46:38 +0700

    x

--- src/pages/HomeNew/tour/index.js
+++ src/pages/HomeNew/tour/index.js
@@ -1,83 +1,79 @@
-import React, { useEffect } from "react"
-import styles from './tour.module.css'
+import React, { useEffect } from "react";
+import styles from "./tour.module.css";
 import { brokenImage } from "../../../assets/images";
 import { useNavigate } from "react-router-dom";
 import { click } from "@testing-library/user-event/dist/click";
 import Slider from "react-slick";
 
+function Tour(props) {
+  const { title, tour, naviga, isLoading } = props;
+  const navigation = useNavigate();
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
+  const handlerBrokenImg = (e) => {
+    e.target.src = brokenImage;
+  };
 
-function Tour(props){
-    const {title,tour,naviga,isLoading}=props
-    const navigation=useNavigate()
-    const  settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          }
-        ]
-      };
-    const handlerBrokenImg = (e) => {
-        e.target.src = brokenImage;
-      };
-    
-
-
-
-
-return(
-  <div className={styles.title}>
-    <div>
+  return (
+    <div className={styles.title}>
+      <div>
         <h3>{title}</h3>
-    </div>
-    <div className={styles.container} >
-    <Slider {...settings}>
-        {!isLoading &&
+      </div>
+      <div className={styles.container}>
+        <Slider {...settings}>
+          {!isLoading &&
             tour &&
             tour.map((item, id) => (
-            <div
-                key={id}
-                className={styles.carouselItem}
-                onClick={() => {}}
-            >
+              <div key={id} className={styles.carouselItemq}>
                 <div className={styles.img}>
-                    <img src={item.thumb} alt={tour.name} onError={handlerBrokenImg} />
+                  <img
+                    src={item.thumb}
+                    alt={tour.name}
+                    onError={handlerBrokenImg}
+                  />
                 </div>
                 <div className={styles.content}>
-                    <h6>item.name</h6>
-                    <ul>
-                        <li>{item.journey}</li>
-                        <li>{item.days+ ' ngày ' + item.nights +' đêm'}</li>
-                        <li>{'Trọn gói:' + item.currentPrice + 'đ'}</li>
-                    </ul>
+                  <h6>item.name</h6>
+                  <ul>
+                    <li>{item.journey}</li>
+                    <li>{item.days + " ngày " + item.nights + " đêm"}</li>
+                    <li>{"Trọn gói:" + item.currentPrice + "đ"}</li>
+                  </ul>
                 </div>
-            </div>
+              </div>
             ))}
-    </Slider>
+        </Slider>
+      </div>
+      <div className={styles.tourdetail} onClick={() => navigation(naviga)}>
+        Xem tất cả
+      </div>
     </div>
-    <div className={styles.tourdetail} onClick={()=>navigation(naviga)}>Xem tất cả</div>
-  </div>
-)
+  );
 }
-export default Tour
-
-
+export default Tour;
